d066a10faa6340a224c8c8231f79077d4b647d2f
dxwwan <kkw1700@gmail.com>
2023-11-21 22:46:43 +0900

    feat(apply, manager): tanstack query 초기 세팅
    Co-authored-by: loopy-lim <loopy@jnu.ac.kr>
    Co-authored-by: 2yunseong <dbsdltjd123@gmail.com>

--- service-manager/src/app/app.tsx
+++ service-manager/src/app/app.tsx
@@ -1,5 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 export function App() {
-  return <div></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
