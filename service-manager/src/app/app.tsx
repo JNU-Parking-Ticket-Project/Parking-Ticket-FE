b89cc00cb35f35c9f0d38bff999f8216b9386197
dxwwan <kkw1700@gmail.com>
2023-11-21 23:42:14 +0900

    feat(apply, manager): react-router-dom 라우트 설정
    Co-authored-by: loopy <loopy-lim@users.noreply.github.com>
    Co-authored-by: 2yunseong <2yunseong@users.noreply.github.com>

--- service-manager/src/app/app.tsx
+++ service-manager/src/app/app.tsx
@@ -1,11 +1,16 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter } from 'react-router-dom';
+import Router from '../router';
 
 const queryClient = new QueryClient();
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
