468382a76e94a0de1cd45a48c51fdd19019d6c92
loopy-lim <loopy@jnu.ac.kr>
2023-11-22 16:51:44 +0900

    feat(apply): Home page에들어갈 constant를 작성한다.
    - common
      - footer
    - main main
      - HOME_TITLE
      - CHONNAM_UNIV
      - INFORMATION
      - NOTICE
      - ADD_MORE
      - LOGIN

---
+++ service-apply/src/constants/common.ts
@@ -0,0 +1,6 @@
+export const FOOTER = {
+  CONTACT_US: 'contact us',
+  STUDENT_COUNCIL_KO: '총학생회',
+  STUDENT_COUNCIL_EN: 'JNU-student council',
+  STUDENT_COUNCIL_PHONE: '010-1234-5678',
+};
---
+++ service-apply/src/constants/home.ts
@@ -0,0 +1,13 @@
+export const HOME_TITLE = '주차권 신청 시스템';
+export const CHONNAM_UNIV = '전남대학교';
+export const INFORMATION = '안내사항';
+export const NOTICE = '공지';
+export const ADD_MORE = '더보기';
+
+export const LOGIN = {
+  TITLE: '신청 폼 작성하기',
+  EMAIL: '이메일',
+  PASSWORD: '비밀번호',
+  FIND_PASSWORD: '비밀번호 찾기',
+  LOGIN: '폼으로 이동',
+};
