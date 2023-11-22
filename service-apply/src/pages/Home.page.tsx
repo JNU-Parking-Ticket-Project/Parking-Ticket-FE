66cbe544e7bfe156c40e4af639dbd3e34d160af3
loopy-lim <loopy@jnu.ac.kr>
2023-11-22 18:33:40 +0900

    refactor(apply): footer css 수정
    hight 가 작아질 시 화면이 달라지는 것을 수정하였습니다.

--- service-apply/src/pages/Home.page.tsx
+++ service-apply/src/pages/Home.page.tsx
@@ -6,15 +6,17 @@ import HomeTitle from '../components/home/Title';
 
 export const HomePage = () => {
   return (
-    <main className="max-w-[1280px] m-auto px-12">
-      <HomeTitle />
-      <div className="flex gap-4">
-        <div className="flex-1">
-          <HomeInforamtion />
-          <HomeNotice />
-        </div>
-        <div className="flex-1">
-          <HomeLogin />
+    <main>
+      <div className="max-w-[1280px] m-auto px-12">
+        <HomeTitle />
+        <div className="flex gap-4">
+          <div className="flex-1">
+            <HomeInforamtion />
+            <HomeNotice />
+          </div>
+          <div className="flex-1">
+            <HomeLogin />
+          </div>
         </div>
       </div>
       <Footer />
