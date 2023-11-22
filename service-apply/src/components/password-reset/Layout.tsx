047db03450ed0313e276ea9f6f9442d3f36985f0
2yunseong <dbsdltjd3701@naver.com>
2023-11-22 21:23:39 +0900

    feat(apply): Layout 추상화 구체화

--- service-apply/src/components/password-reset/Layout.tsx
+++ service-apply/src/components/password-reset/Layout.tsx
@@ -1,7 +1,27 @@
 import { PropsWithChildren } from 'react';
 
 const Layout = ({ children }: PropsWithChildren) => {
-  return <div className="w-screen h-screen flex flex-col">{children}</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <header>
+        <h1 className="text-4xl">전남대학교 주차권 신청 시스템</h1>
+      </header>
+      <main className="flex items-center justify-center h-full">
+        {children}
+      </main>
+      <footer className="w-full border-t flex flex-row-reverse py-4">
+        <div className="pr-8">
+          <h3 className="font-bold text-xl pb-4">Contact Us</h3>
+          <p className="color-[#2B2B39] font-light text-sm pb-2">
+            전남대학교 총학생회 : 062-530-xxxx
+          </p>
+          <p className="color-[#2B2B39] font-light text-sm pb-2">
+            JNU-student council
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
