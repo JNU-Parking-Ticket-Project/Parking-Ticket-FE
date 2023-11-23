0c96915d9e67614c2f414aeaff41c3e050c4958b
loopy-lim <loopy@jnu.ac.kr>
2023-11-23 16:23:22 +0900

    feat(ds): 디자인 시스템의 변경에 따른 button css 변경
    - error라는 컬러를 추가하였습니다.
    - disabled가 있으면 color가 동작하지 않습니다.

--- design-system/src/lib/button/button.tsx
+++ design-system/src/lib/button/button.tsx
@@ -1,26 +1,25 @@
 import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import { clsx } from 'clsx';
 
-/* eslint-disable-next-line */
 export interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     PropsWithChildren {
   className?: string;
   color?: keyof typeof colorType;
   size?: keyof typeof sizeType;
+  disabled?: boolean;
 }
 
 const colorType = {
-  primary:
-    'border-[#16ADEA] bg-[#16ADEA] text-[#FFFFFF] focus:shadow-[0px_0px_4rem_.5rem_#A0C5FF7F] hover:border-[#1678EA7F] active:bg-[#0255D5] disabled:bg-[#A0C5FF7F] disabled:border-none disabled:cursor-not-allowed',
-  secondary:
-    'border-[#1D1D1D] bg-[#1D1D1D] text-[#E7E7E7] focus:shadow-[0px_0px_4rem_.5rem_#A0C5FF7F] hover:border-[#16ADEA] active:bg-[#0255D5] disabled:bg-[#A0C5FF7F] disabled:border-none disabled:cursor-not-allowed',
+  primary: 'bg-[#0255D5] text-[#FFFFFF]',
+  secondary: 'bg-[#D9D9D9]',
+  error: 'bg-[#DC0000] text-[#FFFFFF]',
 };
 
 const sizeType = {
-  small: 'px-4 py-2',
-  medium: 'px-6 py-4',
-  large: 'px-8 py-6',
+  small: 'px-6 py-2',
+  medium: 'px-12 py-4',
+  large: 'px-24 py-6',
 };
 
 export function Button({
@@ -28,14 +27,17 @@ export function Button({
   children,
   color = 'primary',
   size = 'medium',
+  disabled,
   ...props
 }: ButtonProps) {
   return (
     <button
       {...props}
       className={clsx(
-        'rounded-md transition-all duration-150 ease-in-out border-2',
-        colorType[color],
+        'rounded-lg transition-all duration-150 ease-in-out',
+        disabled
+          ? 'bg-[#D9D9D9] text-[#777777] border-none cursor-not-allowed'
+          : colorType[color],
         sizeType[size],
         className,
       )}
