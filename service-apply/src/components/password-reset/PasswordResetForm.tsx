628c0169846fc363c84c3cff6a339a6ec9abd9b1
2yunseong <dbsdltjd3701@naver.com>
2023-11-24 22:32:12 +0900

    refactor(apply): 관심사에 맞게 컴포넌트 분리

---
+++ service-apply/src/components/password-reset/PasswordResetForm.tsx
@@ -0,0 +1,43 @@
+import { InputText, Button } from '@quokka/design-system';
+import { CenterFormContainer } from './CenterFormContainer';
+import { InputFlexWrapper } from './InputFlexWrapper';
+import { usePasswordResetForm } from 'service-apply/src/hooks/password-reset/usePasswordResetForm';
+
+export const PasswordResetForm = () => {
+  const { passwordResetForm, submitChangePassword, handleInput } =
+    usePasswordResetForm();
+
+  return (
+    <CenterFormContainer
+      title="비밀번호 재설정"
+      onSubmit={submitChangePassword}
+    >
+      <InputFlexWrapper>
+        <InputText
+          type="text"
+          className="flex-1"
+          label="비밀번호"
+          labelClassName="w-28"
+          placeholder="새로운 비밀번호 입력"
+          value={passwordResetForm.password}
+          name="password"
+          onChange={handleInput}
+        />
+      </InputFlexWrapper>
+      <InputFlexWrapper>
+        <InputText
+          type="password"
+          className="flex-1"
+          label="비밀번호 재설정"
+          labelClassName="w-28"
+          placeholder="새로운 비밀번호 확인"
+          name="confirmPassword"
+          value={passwordResetForm.confirmPassword}
+        />
+      </InputFlexWrapper>
+      <Button size="small" className="mt-8 w-full">
+        변경하기
+      </Button>
+    </CenterFormContainer>
+  );
+};
--- service-apply/src/pages/PasswordReset.page.tsx
+++ service-apply/src/pages/PasswordReset.page.tsx
@@ -1,49 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { Layout } from '../components/password-reset/Layout';
-import { Button, InputText } from '@quokka/design-system';
-import { InputFlexWrapper } from '../components/password-reset/InputFlexWrapper';
-import { CenterFormContainer } from '../components/password-reset/CenterFormContainer';
-import { usePasswordResetForm } from '../hooks/password-reset/usePasswordResetForm';
+import { PasswordResetForm } from '../components/password-reset/PasswordResetForm';
 
 export const PasswordResetPage = () => {
   // TODO: resetId를 이용해 백엔드에 유효한 resetId인지 검증한 후, 페이지를 보여주어야 함.
   const { resetId } = useParams();
-  const { passwordResetForm, submitChangePassword, handleInput } =
-    usePasswordResetForm();
 
   return (
     <Layout>
-      <CenterFormContainer
-        title="비밀번호 재설정"
-        onSubmit={submitChangePassword}
-      >
-        <InputFlexWrapper>
-          <InputText
-            type="text"
-            className="flex-1"
-            label="비밀번호"
-            labelClassName="w-28"
-            placeholder="새로운 비밀번호 입력"
-            value={passwordResetForm.password}
-            name="password"
-            onChange={handleInput}
-          />
-        </InputFlexWrapper>
-        <InputFlexWrapper>
-          <InputText
-            type="password"
-            className="flex-1"
-            label="비밀번호 재설정"
-            labelClassName="w-28"
-            placeholder="새로운 비밀번호 확인"
-            name="confirmPassword"
-            value={passwordResetForm.confirmPassword}
-          />
-        </InputFlexWrapper>
-        <Button size="small" className="mt-8 w-full">
-          변경하기
-        </Button>
-      </CenterFormContainer>
+      <PasswordResetForm />
     </Layout>
   );
 };
