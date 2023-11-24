9fff8d96c812971d3fec2105bbbe26f6ce887750
2yunseong <dbsdltjd3701@naver.com>
2023-11-24 22:19:37 +0900

    feat(apply): 비밀번호 재설정 상태 선언

--- service-apply/src/pages/PasswordReset.page.tsx
+++ service-apply/src/pages/PasswordReset.page.tsx
@@ -2,21 +2,64 @@ import { useParams } from 'react-router-dom';
 import { Layout } from '../components/password-reset/Layout';
 import { Button, InputText } from '@quokka/design-system';
 import { InputFlexWrapper } from '../components/password-reset/InputFlexWrapper';
-import { useState } from 'react';
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEvent,
+  FormEventHandler,
+  useState,
+} from 'react';
 import { CenterFormContainer } from '../components/password-reset/CenterFormContainer';
 
+interface PasswordResetForm {
+  password: string;
+  confirmPassword: string;
+}
+
 export const PasswordResetPage = () => {
+  // TODO: resetId를
   const { resetId } = useParams();
+
+  const [passwordResetForm, setPasswordResetForm] = useState<PasswordResetForm>(
+    {
+      password: '',
+      confirmPassword: '',
+    },
+  );
+
+  const handleInput: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPasswordResetForm({
+      ...passwordResetForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitChangePassword: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (passwordResetForm.password !== passwordResetForm.confirmPassword) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+    alert('비밀번호 재설정이 완료 되었습니다.');
+    // TODO: navigate homepage
+  };
 
   return (
     <Layout>
-      <CenterFormContainer title="비밀번호 재설정">
+      <CenterFormContainer
+        title="비밀번호 재설정"
+        onSubmit={submitChangePassword}
+      >
         <InputFlexWrapper>
           <InputText
             type="text"
             className="flex-1"
             label="비밀번호"
             labelClassName="w-28"
+            placeholder="새로운 비밀번호 입력"
+            value={passwordResetForm.password}
+            name="password"
+            onChange={handleInput}
           />
         </InputFlexWrapper>
         <InputFlexWrapper>
@@ -25,6 +68,9 @@ export const PasswordResetPage = () => {
             className="flex-1"
             label="비밀번호 재설정"
             labelClassName="w-28"
+            placeholder="새로운 비밀번호 확인"
+            name="confirmPassword"
+            value={passwordResetForm.confirmPassword}
           />
         </InputFlexWrapper>
         <Button size="small" className="mt-8 w-full">
