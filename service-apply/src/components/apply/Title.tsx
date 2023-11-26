597eb58e037b1fe533f890b3fa79fe1c65134f96
dxwwan <kkw1700@gmail.com>
2023-11-26 14:50:20 +0900

    feat(apply): apply 페이지 title추가

---
+++ service-apply/src/components/apply/Title.tsx
@@ -0,0 +1,18 @@
+export const ApplyTitle = () => {
+  return (
+    <header className="mt-20 mb-12">
+      <div className="mb-12 text-[3.5rem] font-semibold text-[#0B0B0B] my-4">
+        전남대학교 주차권 신청 시스템
+      </div>
+      <div className="flex flex-row items-center">
+        <h1 className="mr-[45rem] text-5xl font-semibold text-[#0B0B0B]">
+          주차권 신청
+        </h1>
+        <div className="text-xl text-[#0B0B0B]">신청기간: 2023년 12월 31일</div>
+      </div>
+      <hr className="mt-3 border-solid bg-black h-1" />
+    </header>
+  );
+};
+
+export default ApplyTitle;
