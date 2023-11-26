621578bad4e177dbcf600032e8763c4b6fb5a230
dxwwan <kkw1700@gmail.com>
2023-11-26 14:49:48 +0900

    feat(apply): 구간 상수 추가

---
+++ service-apply/src/components/constants/parkingSection.ts
@@ -0,0 +1,16 @@
+interface ParkingSection {
+  number: number;
+  name: string[];
+}
+
+export const parkingSection: ParkingSection[] = [
+  {
+    number: 1,
+    name: ['공과대학', '의치대 1학년'],
+  },
+  {
+    number: 2,
+    name: ['경영대학', '본부직할'],
+  },
+  //TODO section 추가
+];
