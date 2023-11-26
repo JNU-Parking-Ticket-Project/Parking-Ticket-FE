cd1df721cfd418bd5ba74debcb6249a85cebffde
dxwwan <kkw1700@gmail.com>
2023-11-26 16:59:09 +0900

    fix(apply): export문 컨벤션에 맞게 수정

--- service-apply/src/components/apply/Title.tsx
+++ service-apply/src/components/apply/Title.tsx
@@ -14,5 +14,3 @@ export const ApplyTitle = () => {
     </header>
   );
 };
-
-export default ApplyTitle;
--- service-apply/src/pages/Apply.page.tsx
+++ service-apply/src/pages/Apply.page.tsx
@@ -1,19 +1,15 @@
-import ApplyForm from '../components/apply/Form';
-import Title from '../components/apply/Title';
-import Footer from '../components/common/Footer';
+import { ApplyForm } from '../components/apply/Form';
+import { ApplyTitle } from '../components/apply/Title';
+import { Footer } from '../components/common/Footer';
 
 export const ApplyPage = () => {
   return (
     <main>
       <div className="max-w-[1280px] m-auto px-12">
-        <div>
-          <Title />
-          <ApplyForm />
-        </div>
+        <ApplyTitle />
+        <ApplyForm />
       </div>
       <Footer />
     </main>
   );
 };
-
-export default ApplyPage;
