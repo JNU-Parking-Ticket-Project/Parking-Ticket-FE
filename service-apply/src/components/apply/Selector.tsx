d5fb4a14eaf082b5cc3e7ebafee1422a24a0ea0f
dxwwan <kkw1700@gmail.com>
2023-11-26 16:58:42 +0900

    fix(apply): SelecorProps 컨벤션에 맞게 수정

--- service-apply/src/components/apply/Selector.tsx
+++ service-apply/src/components/apply/Selector.tsx
@@ -1,13 +1,13 @@
 import { SelectHTMLAttributes } from 'react';
 import { parkingSection } from '../constants/parkingSection';
 
-interface selectorProps extends SelectHTMLAttributes<HTMLSelectElement> {
+interface SelectorProps extends SelectHTMLAttributes<HTMLSelectElement> {
   label: string;
   type: string;
   options: typeof parkingSection;
 }
 
-export const Selector = ({ label, type, options, ...props }: selectorProps) => {
+export const Selector = ({ label, type, options, ...props }: SelectorProps) => {
   return (
     <div className="flex justify-between">
       <label htmlFor={label}>{label}</label>
@@ -17,7 +17,7 @@ export const Selector = ({ label, type, options, ...props }: selectorProps) => {
         className="p-2 w-[22rem] border border-[#D9D9D9] "
       >
         {options.map((options) => (
-          <option value={options.number}>{options.name}</option>
+          <option value={options.sectionNumber}>{options.sectionMajor}</option>
         ))}
       </select>
     </div>
