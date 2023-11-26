31ef3d9460054b9b0807b81dab0790c16389d890
dxwwan <kkw1700@gmail.com>
2023-11-26 14:49:32 +0900

    feat(apply): 라우터에 apply페이지 추가

--- service-apply/src/router/index.tsx
+++ service-apply/src/router/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from '../pages/Home.page';
+import { ApplyPage } from '../pages/Apply.page';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/apply" element={<ApplyPage />} />
     </Routes>
   );
 }
