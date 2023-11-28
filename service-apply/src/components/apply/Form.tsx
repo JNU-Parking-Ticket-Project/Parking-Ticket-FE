76f3acef52275482af08fe055315bc8e70920c6b
dxwwan <kkw1700@gmail.com>
2023-11-28 21:21:58 +0900

    refactor(apply): 신청 페이지 css 리팩토링

--- service-apply/src/components/apply/Form.tsx
+++ service-apply/src/components/apply/Form.tsx
@@ -6,83 +6,82 @@ import {
 } from '@quokka/design-system';
 import { Selector } from './Selector';
 import { parkingSection } from '../constants/parkingSection';
+import { clsx } from 'clsx';
 
-export const ApplyInputText = (props: InputTextProps) => {
+export const ApplyInputText = ({ className, ...props }: InputTextProps) => {
   return (
-    <div className="gap-3 flex flex-row justify-between items-center mb-2">
-      <InputText {...props} className={`${props.className}`} />
+    <div className="gap-3 grid grid-cols-5 justify-between items-center mb-2">
+      <InputText {...props} className={clsx(className, 'col-span-4')} />
     </div>
   );
 };
 
 export const ApplyForm = () => {
   return (
-    <div className="flex justify-center">
-      <form className="grid gap-4 w-[35rem] mb-[3.6rem]">
-        <ApplyInputText
-          label="전화번호"
-          placeholder="010-0000-0000"
-          type="text"
-          className="w-[30rem]"
-          required
-        />
-        <ApplyInputText
-          label="이메일"
-          placeholder=""
-          type="text"
-          className="w-[30rem]"
-          required
-        />
-        <ApplyInputText
-          label="이름"
-          placeholder=""
-          type="text"
-          className="w-[30rem]"
-          required
-        />
-        <ApplyInputText
-          label="학번"
-          placeholder=""
-          type="text"
-          className="w-[30rem]"
-          required
-        />
-        <ApplyInputText
-          label="소속 대학"
-          placeholder=""
-          type="text"
-          className="w-[30rem]"
-          required
-        />
-        <Selector
-          label="구간"
-          placeholder=""
-          type="text"
-          options={parkingSection}
-          className="w-[30rem]"
-          required
-        />
-        <ApplyInputText
-          label="차량 번호"
-          placeholder=""
-          type="text"
-          className="w-[30rem]"
-          required
-        />
-        <div className="flex flex-row justify-between mb-[3.5rem]">
-          <label>경차 여부</label>
-          <CheckBox label="예" name="compactCar" value="yes" required />
-          <CheckBox label="아니오" name="compactCar" value="no" required />
-        </div>
-        <div className="flex flex-row justify-between">
-          <Button className="w-[13rem]" color={'secondary'}>
-            임시 저장
-          </Button>
-          <Button className="w-[13rem]" color={'primary'} type="submit">
-            신청하기
-          </Button>
-        </div>
-      </form>
-    </div>
+    <form className="flex flex-col gap-4 w-[480px] m-auto">
+      <ApplyInputText
+        label="전화번호"
+        placeholder="010-0000-0000"
+        type="text"
+        className=""
+        required
+      />
+      <ApplyInputText
+        label="이메일"
+        placeholder=""
+        type="text"
+        className=""
+        required
+      />
+      <ApplyInputText
+        label="이름"
+        placeholder=""
+        type="text"
+        className=""
+        required
+      />
+      <ApplyInputText
+        label="학번"
+        placeholder=""
+        type="text"
+        className=""
+        required
+      />
+      <ApplyInputText
+        label="소속 대학"
+        placeholder=""
+        type="text"
+        className=""
+        required
+      />
+      <Selector
+        label="구간"
+        placeholder=""
+        type="text"
+        options={parkingSection}
+        className=""
+        required
+      />
+      <ApplyInputText
+        label="차량 번호"
+        placeholder=""
+        type="text"
+        className=""
+        required
+      />
+      <div className="flex flex-row justify-between mb-[3.5rem]">
+        <div>경차 여부</div>
+        <CheckBox label="예" name="compactCar" value="yes" required />
+        <CheckBox label="아니오" name="compactCar" value="no" required />
+      </div>
+      <div className="flex flex-row justify-between">
+        <Button className="" color="secondary">
+          임시 저장
+        </Button>
+        <Button className="" color="primary" type="submit">
+          신청하기
+        </Button>
+      </div>
+    </form>
   );
 };
--- service-apply/src/components/apply/Selector.tsx
+++ service-apply/src/components/apply/Selector.tsx
@@ -1,4 +1,4 @@
-import { SelectHTMLAttributes } from 'react';
+import { SelectHTMLAttributes, useId } from 'react';
 import { parkingSection } from '../constants/parkingSection';
 
 interface SelectorProps extends SelectHTMLAttributes<HTMLSelectElement> {
@@ -8,13 +8,15 @@ interface SelectorProps extends SelectHTMLAttributes<HTMLSelectElement> {
 }
 
 export const Selector = ({ label, type, options, ...props }: SelectorProps) => {
+  const id = useId();
+
   return (
-    <div className="flex justify-between">
-      <label htmlFor={label}>{label}</label>
+    <div className="grid grid-cols-5 justify-between">
+      <label htmlFor={id}>{label}</label>
       <select
         {...props}
-        id={label}
-        className="p-2 w-[22rem] border border-[#D9D9D9] "
+        id={id}
+        className="p-2  border border-[#D9D9D9] col-span-4"
       >
         {options.map((options) => (
           <option value={options.sectionNumber}>{options.sectionMajor}</option>
