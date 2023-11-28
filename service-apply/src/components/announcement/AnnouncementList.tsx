7210c92bc3cc9c3c611d7ee21e63006a08fa9412
2yunseong <dbsdltjd3701@naver.com>
2023-11-28 16:23:32 +0900

    feat(apply): 더미데이터 추가

--- service-apply/src/components/announcement/AnnouncementList.tsx
+++ service-apply/src/components/announcement/AnnouncementList.tsx
@@ -1,5 +1,47 @@
 import { AnnouncementListItem } from './AnnouncementListItem';
 
+const datas = [
+  {
+    id: 1,
+    title: '전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인',
+    date: '11-26',
+  },
+  {
+    id: 2,
+    title: '전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인',
+    date: '11-26',
+  },
+  {
+    id: 3,
+    title: '전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인',
+    date: '11-26',
+  },
+  {
+    id: 4,
+    title: '전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인',
+    date: '11-26',
+  },
+  {
+    id: 5,
+    title: '전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인',
+    date: '11-26',
+  },
+  {
+    id: 6,
+    title: '전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인',
+    date: '11-26',
+  },
+  {
+    id: 7,
+    title: '전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인',
+    date: '11-26',
+  },
+  {
+    id: 8,
+    title: '전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인',
+    date: '11-26',
+  },
+];
 export const AnnouncementList = () => {
   // TODO: 공지사항 목록 map으로 처리하는데, query를 가급적 이 부분에서 사용하면 좋다고 생각합니다.
   return (
@@ -11,30 +53,9 @@ export const AnnouncementList = () => {
         </tr>
       </thead>
       <tbody>
-        <AnnouncementListItem
-          title="전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인"
-          date="11-26"
-        />
-        <AnnouncementListItem
-          title="전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인"
-          date="11-26"
-        />
-        <AnnouncementListItem
-          title="전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인"
-          date="11-26"
-        />
-        <AnnouncementListItem
-          title="전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인"
-          date="11-26"
-        />
-        <AnnouncementListItem
-          title="전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인"
-          date="11-26"
-        />
-        <AnnouncementListItem
-          title="전남대학교 주차권 1차 신청일은 12-03일 입니다. 아래 공지에서 확인"
-          date="11-26"
-        />
+        {datas.map(({ id, ...props }) => (
+          <AnnouncementListItem key={id} {...props} />
+        ))}
       </tbody>
     </table>
   );
