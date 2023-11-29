db045d9bb0857f1ce4ef34903e2462ca822a12cd
loopy-lim <loopy@jnu.ac.kr>
2023-11-29 23:54:23 +0900

    feat(apply): pagenav generatePaginationIndexs를 만든다

--- service-apply/src/components/announcement/PageNav.tsx
+++ service-apply/src/components/announcement/PageNav.tsx
@@ -1,70 +1,89 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { generatePaginationIndexs } from '../../constants/announcement';
-import clsx from 'clsx';
+import { Link } from 'react-router-dom';
 import { Icon } from './Icon';
 import { Txt } from '@quokka/design-system';
+import { generatePaginationIndexs } from '../../functions/anouncement';
 
 interface PageNavProps {
   lastIdx: number;
   currentIdx: number;
 }
 
+const PrevNavLinkGroup = ({ currentIdx }: { currentIdx: number }) => {
+  if (currentIdx > 1) {
+    return (
+      <>
+        <Link className="p-1" to={`/announce?pages=${1}`}>
+          <Icon type="rightDoubleArrow" className="rotate-180" />
+        </Link>
+        <Link to={`/announce?pages=${currentIdx - 1}`} className="px-1">
+          <Icon type="rightArrow" className="rotate-180" />
+        </Link>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="cursor-not-allowed p-1">
+        <Icon type="rightDoubleArrow" color="disabled" className="rotate-180" />
+      </div>
+      <div className="cursor-not-allowed p-1">
+        <Icon type="rightArrow" color="disabled" className="rotate-180" />
+      </div>
+    </>
+  );
+};
+
+const NextNavLinkGroup = ({
+  currentIdx,
+  lastIdx,
+}: {
+  currentIdx: number;
+  lastIdx: number;
+}) => {
+  if (currentIdx < lastIdx) {
+    return (
+      <>
+        <Link to={`/announce?pages=${currentIdx + 1}`} className="px-1">
+          <Icon type="rightArrow" />
+        </Link>
+        <Link className="px-1" to={`/announce?pages=${lastIdx}`}>
+          <Icon type="rightDoubleArrow" />
+        </Link>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="cursor-not-allowed p-1">
+        <Icon type="rightDoubleArrow" color="disabled" />
+      </div>
+      <div className="cursor-not-allowed p-1">
+        <Icon type="rightArrow" color="disabled" />
+      </div>
+    </>
+  );
+};
+
 export const PageNav = ({ lastIdx, currentIdx }: PageNavProps) => {
   return (
     <div className="max-w-[768px] w-full mx-auto my-8">
       <div className="flex items-center justify-end">
-        {currentIdx !== 1 ? (
-          <>
-            <Link className="p-1" to={`/announce?pages=${1}`}>
-              <Icon type="rightDoubleArrow" className="rotate-180" />
-            </Link>
-            <Link to={`/announce?pages=${currentIdx - 1}`} className="px-1">
-              <Icon type="rightArrow" className="rotate-180" />
-            </Link>
-          </>
-        ) : (
-          <>
-            <div className="cursor-not-allowed p-1">
-              <Icon
-                type="rightDoubleArrow"
-                color="disabled"
-                className="rotate-180"
-              />
-            </div>
-            <div className="cursor-not-allowed p-1">
-              <Icon type="rightArrow" color="disabled" className="rotate-180" />
-            </div>
-          </>
-        )}
+        <PrevNavLinkGroup currentIdx={currentIdx} />
         {generatePaginationIndexs(currentIdx, lastIdx).map((page) => (
-          <Link key={page} to={`/announce?pages=${page}`}>
+          <Link
+            key={page}
+            to={`/announce?pages=${page}`}
+            className="w-6 text-center"
+          >
             <Txt
               color={page === currentIdx ? 'black' : 'placeholder'}
-              className="text-xl p-2"
+              className="text-xl"
             >
               {page}
             </Txt>
           </Link>
         ))}
-        {currentIdx !== lastIdx ? (
-          <>
-            <Link to={`/announce?pages=${currentIdx + 1}`} className="px-1">
-              <Icon type="rightArrow" />
-            </Link>
-            <Link className="px-1" to={`/announce?pages=${lastIdx}`}>
-              <Icon type="rightDoubleArrow" />
-            </Link>
-          </>
-        ) : (
-          <>
-            <div className="cursor-not-allowed p-1">
-              <Icon type="rightDoubleArrow" color="disabled" />
-            </div>
-            <div className="cursor-not-allowed p-1">
-              <Icon type="rightArrow" color="disabled" />
-            </div>
-          </>
-        )}
+        <NextNavLinkGroup currentIdx={currentIdx} lastIdx={lastIdx} />
       </div>
     </div>
   );
