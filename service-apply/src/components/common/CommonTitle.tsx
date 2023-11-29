60b11db58ea01e5a8fbffb758e6d49c7f826e6a3
loopy-lim <loopy@jnu.ac.kr>
2023-11-29 23:58:53 +0900

    feat(apply): title을 누르면 홈으로 간다

--- service-apply/src/components/common/CommonTitle.tsx
+++ service-apply/src/components/common/CommonTitle.tsx
@@ -1,10 +1,13 @@
 import { Txt } from '@quokka/design-system';
+import { Link } from 'react-router-dom';
 
 export const CommonTitle = () => {
   return (
     <header className="mt-36 mb-12">
       <h1>
-        <Txt size="h2">전남대학교 주차권 신청 시스템</Txt>
+        <Link to="/">
+          <Txt size="h2">전남대학교 주차권 신청 시스템</Txt>
+        </Link>
       </h1>
     </header>
   );
