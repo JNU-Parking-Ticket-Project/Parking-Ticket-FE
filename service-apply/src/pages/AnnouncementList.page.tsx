1f0ca7f3afba67565f679d8833c1571edd5a3bb0
loopy-lim <loopy@jnu.ac.kr>
2023-11-29 13:47:30 +0900

    refactor(apply): common component에서 가지고 와 변경한다

--- service-apply/src/pages/AnnouncementList.page.tsx
+++ service-apply/src/pages/AnnouncementList.page.tsx
@@ -1,9 +1,11 @@
 import { Txt } from '@quokka/design-system';
 import Footer from '../components/common/Footer';
 import { AnnouncementList } from '../components/announcement/AnnouncementList';
-import { Navigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useMemo } from 'react';
 import { PageNav } from '../components/announcement/PageNav';
+import { CommonTitle } from '../components/common/CommonTitle';
+import { MainContainer } from '../components/common/MainConatiner';
 
 const useQueryParameter = () => {
   const { search } = useLocation();
@@ -12,27 +14,20 @@ const useQueryParameter = () => {
 
 export const AnnouncementListPage = () => {
   const query = useQueryParameter();
-  const pages = query.get('pages');
-  if (!pages) {
-    return <Navigate replace to="/announcement?pages=1" />;
-  }
+  const pages = query.get('pages') ?? 1;
 
   return (
-    <div className="absolute h-full w-full left-0 top-0">
-      <header>
-        <h1>
-          <Txt size="h3">전남대학교 주차권 신청 시스템</Txt>
-        </h1>
-      </header>
-      <main className="h-full w-full px-24 py-12">
+    <>
+      <MainContainer>
+        <CommonTitle />
         <Txt size="h4" color="primay">
           공지사항
         </Txt>
         <hr className="mt-3 border-solid bg-black h-1" />
         <AnnouncementList />
         <PageNav lastIdx={10} currentIdx={+pages} />
-      </main>
+      </MainContainer>
       <Footer />
-    </div>
+    </>
   );
 };
