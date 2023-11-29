7ff13704269892d8c41a0f58287c8ad92081eea3
loopy-lim <loopy@jnu.ac.kr>
2023-11-29 13:44:32 +0900

    feat(apply): common title을 만든다

---
+++ service-apply/src/components/common/CommonTitle.tsx
@@ -0,0 +1,11 @@
+import { Txt } from '@quokka/design-system';
+
+export const CommonTitle = () => {
+  return (
+    <header className="mt-36 mb-12">
+      <h1>
+        <Txt size="h3">전남대학교 주차권 신청 시스템</Txt>
+      </h1>
+    </header>
+  );
+};
