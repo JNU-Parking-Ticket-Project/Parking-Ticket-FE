2cb66ddacc41426fd41f11c83faf85e9b60cb6dd
dxwwan <kkw1700@gmail.com>
2023-11-29 09:28:00 +0900

    rafactor(apply): 구간에 디폴트값 추가, 구간의 프로퍼티명 수정

--- service-apply/src/components/constants/parkingSection.ts
+++ service-apply/src/components/constants/parkingSection.ts
@@ -1,16 +1,20 @@
 interface ParkingSection {
   sectionNumber: number;
-  sectionMajor: string[];
+  sectionMajor: string;
 }
 
 export const parkingSection: ParkingSection[] = [
   {
+    sectionNumber: 0,
+    sectionMajor: '없음',
+  },
+  {
     sectionNumber: 1,
-    sectionMajor: ['공과대학', '의치대 1학년'],
+    sectionMajor: '공과대학, 의치대 1학년',
   },
   {
     sectionNumber: 2,
-    sectionMajor: ['경영대학', '본부직할'],
+    sectionMajor: '경영대학, 본부직할',
   },
   //TODO: section 추가
 ];
