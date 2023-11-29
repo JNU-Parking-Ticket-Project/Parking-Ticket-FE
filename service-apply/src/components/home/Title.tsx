1b8d07bd68c5778da0dec52a1ffe543f8e43d1ab
loopy-lim <loopy@jnu.ac.kr>
2023-11-30 00:45:10 +0900

    refactor(apply): home title css 를 수정한다(announcement title과 맞추기 위함)

--- service-apply/src/components/home/Title.tsx
+++ service-apply/src/components/home/Title.tsx
@@ -3,7 +3,7 @@ import { Txt } from '@quokka/design-system';
 export const HomeTitle = () => {
   return (
     <header className="pt-36 pb-12">
-      <Txt size="h2" className="py-4 block">
+      <Txt size="h2" className="pb-4 block">
         전남대학교
       </Txt>
       <Txt size="h1">주차권 신청 시스템</Txt>
