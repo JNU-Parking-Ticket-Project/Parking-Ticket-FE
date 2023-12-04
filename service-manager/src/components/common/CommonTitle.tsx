5866a304a7ce9d2ace25903e4b4ed8e261c491c9
dxwwan <kkw1700@gmail.com>
2023-12-04 10:54:40 +0900

    feat(manager): 공용 컴포넌트 생성

---
+++ service-manager/src/components/common/CommonTitle.tsx
@@ -0,0 +1,14 @@
+import { Txt } from '@quokka/design-system';
+import { Link } from 'react-router-dom';
+
+export const CommonTitle = () => {
+  return (
+    <header className="pt-36 pb-12">
+      <h1>
+        <Link to="/">
+          <Txt size="h2">전남대학교 주차권 신청 시스템</Txt>
+        </Link>
+      </h1>
+    </header>
+  );
+};
---
+++ service-manager/src/components/common/NavTitle.tsx
@@ -0,0 +1,10 @@
+import { Txt } from '@quokka/design-system';
+
+export const NavTitle = () => {
+  return (
+    <header className="flex flex-col text py-5 px-12">
+      <Txt size="h3">전남대학교</Txt>
+      <Txt size="h6">주차권 신청 시스템</Txt>
+    </header>
+  );
+};
