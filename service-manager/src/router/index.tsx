b99af95efd0f617ba876d700cbb2f8790cb0400f
dxwwan <kkw1700@gmail.com>
2023-12-04 16:25:36 +0900

    feat(manager): 비밀번호 재설정 페이지 추가(신청 도메인 재사용)

--- service-manager/src/router/index.tsx
+++ service-manager/src/router/index.tsx
@@ -1,4 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import { MainPage } from '../pages/Main.page';
+import { SignUpPage } from '../pages/SignUp.page';
+import { MainUserPage } from '../pages/Main.user';
+import { AnnouncePage } from '../pages/Announce.page';
+import { ApplyListPage } from '../pages/ApplyList.page';
+import { PasswordResetPage } from '../pages/PasswordReset.page';
+import { NoticeView } from '../pages/notice/NoticeView.page';
+import { NoticeCreate } from '../pages/notice/NoticeCreate.page';
 
 export default function Router() {
   return (
@@ -8,6 +16,7 @@ export default function Router() {
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/announce" element={<AnnouncePage />} />
       <Route path="/applyList" element={<ApplyListPage />} />
+      <Route path="/password-reset" element={<PasswordResetPage />} />
       <Route path="/notice" element={<NoticeView />} />
       <Route path="/notice-create" element={<NoticeCreate />} />
     </Routes>
