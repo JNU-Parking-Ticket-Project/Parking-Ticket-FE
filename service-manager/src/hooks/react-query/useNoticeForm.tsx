f54828e233bf24d7698c3065d27102985ac41fc8
dxwwan <kkw1700@gmail.com>
2023-12-10 16:33:52 +0900

    feat(manager): 안내사항 cru 훅 추가

---
+++ service-manager/src/hooks/react-query/useNoticeForm.tsx
@@ -0,0 +1,69 @@
+import { NoticeFormProps } from 'service-manager/src/components/notice/NoticeForm';
+import { useNoticeMutate, useNoticeQuery } from '../useNotice';
+import { useNavigate } from 'react-router-dom';
+import { useReducer } from 'react';
+
+type NoticeFormInputAction =
+  | {
+      type: keyof NoticeFormProps;
+      payload: NoticeFormProps[keyof NoticeFormProps];
+    }
+  | {
+      type: 'reset';
+      payload: null;
+    };
+
+const noticeFormReducer = (
+  state: NoticeFormProps,
+  action: NoticeFormInputAction,
+): NoticeFormProps => {
+  switch (action.type) {
+    case 'content':
+      return {
+        ...state,
+        content: action.payload,
+      };
+    case 'reset':
+      return {
+        ...state,
+        ...initValue,
+      };
+    default:
+      return state;
+  }
+};
+
+const initValue = {
+  content: '',
+};
+
+export const useNoticeForm = (init?: NoticeFormProps) => {
+  init ||= initValue;
+  const [state, dispatch] = useReducer(noticeFormReducer, init);
+  const { putNotice } = useNoticeMutate();
+  const navigate = useNavigate();
+
+  const onSubmit = () => {
+    putNotice(
+      { noticeContent: state.content },
+      {
+        onError: (error) => {
+          console.log(error);
+          alert(error.message);
+          throw new Error(error.message);
+        },
+        onSuccess: (data) => {
+          if (!data) throw new Error('data is undefined');
+          dispatch({ type: 'reset', payload: null });
+          alert('안내사항이 수정되었습니다.');
+          navigate('/');
+        },
+      },
+    );
+  };
+  return {
+    state,
+    dispatch,
+    onSubmit,
+  };
+};
---
+++ service-manager/src/hooks/useNotice.tsx
@@ -0,0 +1,35 @@
+import { getNotice, putNotice } from '../apis/notice.apis';
+import { useQuery, useMutation, MutateOptions } from '@tanstack/react-query';
+import { Notice } from '../apis/dtos/notice.dtos';
+
+export const useNoticeQuery = () => {
+  const { data } = useQuery({
+    queryKey: ['notice'],
+    queryFn: getNotice,
+    gcTime: Infinity,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  });
+  return { noticeData: data };
+};
+
+export const useNoticeMutate = () => {
+  const { mutate } = useMutation({
+    mutationKey: ['notice'],
+    mutationFn: putNotice,
+  });
+
+  return {
+    putNotice: (
+      content: Notice,
+      mutateOption?: Omit<MutateOptions<Notice, Error, unknown>, 'onSettled'>,
+    ) => {
+      mutate(content, {
+        ...mutateOption,
+        onSettled: (data) => {
+          if (!data) throw new Error('data is undefined');
+        },
+      });
+    },
+  };
+};
