2c0a8538427c7879477713e744889c6dec09fb20
dxwwan <kkw1700@gmail.com>
2023-12-13 20:17:28 +0900

    style(manager): 안내사항 페이지 스타일 변경

--- service-manager/src/pages/notice/NoticeView.page.tsx
+++ service-manager/src/pages/notice/NoticeView.page.tsx
@@ -2,7 +2,7 @@ import { NoticeView } from '../../components/notice/NoticeView';
 
 export const NoticeViewPage = () => {
   return (
-    <div className="flex flex-col">
+    <div className="max-w-[786px] mx-auto mt-12 w-full">
       <NoticeView />
     </div>
   );
