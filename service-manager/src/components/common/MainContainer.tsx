117cc21495cabef6dc7fa46ecc56abf0371863a7
2yunseong <dbsdltjd3701@naver.com>
2023-12-15 22:17:38 +0900

    feat(workspace): 백그라운드 이미지 추가

--- service-manager/src/components/common/MainContainer.tsx
+++ service-manager/src/components/common/MainContainer.tsx
@@ -1,10 +1,22 @@
 import { PropsWithChildren } from 'react';
+import Background from '../../assets/background.png';
+import clsx from 'clsx';
+import { useLocation } from 'react-router-dom';
 
 interface MainContainerProps extends PropsWithChildren {}
 
 export const MainContainer = ({ children }: MainContainerProps) => {
+  const location = useLocation();
+
   return (
     <main className="min-h-[calc(100dvh-11rem)] max-w-[1600px] m-auto px-12">
+      <img
+        src={Background}
+        className={clsx(
+          'absolute right-0 top-0 -z-50',
+          location.pathname !== '/' && 'opacity-[.43]',
+        )}
+      />
       {children}
     </main>
   );
