eb16d4a1a95f6cf70ee9d6b1ffbc80765dfbcf67
2yunseong <dbsdltjd3701@naver.com>
2023-12-15 21:49:02 +0900

    test(apply): 함수 변경에 따른 테스트 수정

--- service-apply/src/__test__/date.test.ts
+++ service-apply/src/__test__/date.test.ts
@@ -1,7 +1,7 @@
-import { parseISO8601ToFormatString } from '../functions/date';
+import { getApplyDateString } from '../functions/date';
 
 test('ISO 8601 날짜 형식을 YYYY.MM.DD HH:MM:SS 로 변환한다.', () => {
-  expect(parseISO8601ToFormatString('2023-12-14T00:30:00')).toEqual(
-    '2023.12.14 00:30:00',
+  expect(getApplyDateString(new Date('2023-12-14T00:30:00'))).toEqual(
+    '시작시간: 2023.12.14 00:30:00',
   );
 });
