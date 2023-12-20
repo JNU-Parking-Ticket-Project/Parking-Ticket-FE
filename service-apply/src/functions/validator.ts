81e5a7b0d90cdb72544fa44050071cbe3ac820a3
j8won <joowoni99@gmail.com>
2023-12-20 20:18:29 +0900

    feat(apply): 전화번호, 학번, 이름, 구간, 차량번호, 소속대학, 비밀번호, apply form, password reset form의 validator 함수 추가

--- service-apply/src/functions/validator.ts
+++ service-apply/src/functions/validator.ts
@@ -1,7 +1,7 @@
-import { submitFailure, submitSucccess } from './rop';
+import { submitFailure, submitSuccess } from './rop';
 
 export const isPhoneNumber = (phoneNumber: string) =>
-  !/^01[0-9]{1}-[0-9]{4}-[0-9]{4}$/.test(phoneNumber);
+  /^01[0-9]{1}-[0-9]{4}-[0-9]{4}$/.test(phoneNumber);
 
 export const isEmail = (email: string) =>
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
@@ -9,18 +9,27 @@ export const isEmail = (email: string) =>
   );
 
 export const isStudentNumber = (studentNumber: string) =>
-  /^[0-9]{6}$/.test(studentNumber.toString());
+  /^[0-9]{6}$/.test(studentNumber);
 
 export const isStudentName = (studentName: string) =>
-  /^[가-힣]{3,5}$/.test(studentName);
+  /^[가-힣]{2,5}$/.test(studentName);
 
-export const isSection = (section: number) => {
-  //TODO: section 검증
-  return true;
+export const isSection = ({
+  array,
+  selected,
+}: {
+  array: number[];
+  selected: number;
+}) => {
+  if (array.includes(selected)) return true;
+  return false;
 };
 
-export const isCarNumber = (carNumber: string) => {
-  //TODO: 차량번호 검증
+export const isCarNumber = (carNumber: string) =>
+  /\d{2,3}[가-힣]{1}\d{4}/gm.test(carNumber.replace(' ', ''));
+
+export const isAffiliation = (affiliation: string) => {
+  if (!affiliation.replace(' ', '')) return false;
   return true;
 };
 
@@ -35,7 +44,18 @@ export interface ApplyFormInput {
   isCompact: boolean;
 }
 
-export const applyFormValidator = (input: ApplyFormInput) => {
+export const applyFormValidator = ({
+  input,
+  sectionNumberArray,
+  isAgreed,
+}: {
+  input: ApplyFormInput;
+  sectionNumberArray: number[];
+  isAgreed: boolean;
+}) => {
+  if (!isAgreed) {
+    return submitFailure('개인정보 수집 및 이용에 동의해 주세요.');
+  }
   if (!isPhoneNumber(input.phoneNumber)) {
     return submitFailure('올바른 전화번호가 아닙니다.');
   }
@@ -46,10 +66,36 @@ export const applyFormValidator = (input: ApplyFormInput) => {
     return submitFailure('올바른 이름이 아닙니다.');
   }
   if (!isStudentNumber(input.studentNumber)) {
-    return submitFailure('올바른 학번이 아닙니다.');
+    return submitFailure('학번은 6자리 숫자입니다.');
   }
-  if (!isSection(input.section)) {
-    return submitFailure('올바른 구역이 아닙니다.');
+  if (!isAffiliation(input.affiliation)) {
+    return submitFailure('소속대학을 입력해 주세요');
   }
-  return submitSucccess(input);
+  if (!isSection({ array: sectionNumberArray, selected: input.section })) {
+    return submitFailure('올바른 구간이 아닙니다.');
+  }
+  if (!isCarNumber(input.carNumber)) {
+    return submitFailure('차량 번호를 양식에 맞게 입력해 주세요.');
+  }
+  return submitSuccess(input);
+};
+
+export const isPassword = (password: string) =>
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,20}$/.test(password);
+
+export interface PasswordResetFormInput {
+  password: string;
+  confirmPassword: string;
+}
+
+export const passwordResetFormValidator = (input: PasswordResetFormInput) => {
+  if (!isPassword(input.password)) {
+    return submitFailure(
+      '비밀번호는 대문자, 소문자, 숫자, 특수문자를 모두 포함한 8자리 이상이어야 합니다.',
+    );
+  }
+  if (input.password !== input.confirmPassword) {
+    return submitFailure('비밀번호가 일치하지 않습니다');
+  }
+  return submitSuccess(input);
 };
