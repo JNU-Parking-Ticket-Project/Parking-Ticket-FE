55647f81003a0954b08562c3307fc101e0d5cf7e
j8won <joowoni99@gmail.com>
2023-12-21 23:40:04 +0900

    feat(apply): apply form validation 안내문 추가

--- service-apply/src/components/apply/ApplyTitle.tsx
+++ service-apply/src/components/apply/ApplyTitle.tsx
@@ -2,11 +2,17 @@ import { Txt } from '@quokka/design-system';
 
 export const ApplyTitle = () => {
   return (
-    <h2 className="border-b-4 border-black mt-3 pb-2">
-      <Txt size="h3" color="primary" className="mr-5">
-        주차권 신청
+    <>
+      <h2 className="border-b-4 border-black mt-3 mb-2 pb-2">
+        <Txt size="h3" color="primary" className="mr-5">
+          주차권 신청
+        </Txt>
+        <Txt>신청 기간 : 2023년 12월 15일 14:00</Txt>
+      </h2>
+      <Txt color="error">
+        적합한 서식이 아니라면 신청할 수 없습니다. 임시저장을 통해 서식을 확인해
+        주세요.
       </Txt>
-      <Txt>신청 기간 : 2023년 12월 15일 14:00</Txt>
-    </h2>
+    </>
   );
 };
