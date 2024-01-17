4771b33161b0994f5e448fa189657223ddafe40f
dxwwan <kkw1700@gmail.com>
2024-01-17 18:01:00 +0900

    feat(manager): 빌드오류 수정

--- service-manager/src/hooks/useLogout.tsx
+++ service-manager/src/hooks/useLogout.tsx
@@ -1,22 +1,18 @@
-import { useLogoutMutate } from "./react-query/useUser";
-import { useNavigate } from "react-router-dom";
-import { removeToken } from "../functions/jwt";
-import { UserToken } from "../apis/dtos/user.dtos";
+import { useLogoutMutate } from './react-query/useUser';
+import { useNavigate } from 'react-router-dom';
+import { removeToken } from '../functions/jwt';
 
 export const useLogout = () => {
-    const { postLogout } = useLogoutMutate();
-    const navigate = useNavigate();
+  const { postLogout } = useLogoutMutate();
+  const navigate = useNavigate();
 
-    const onLogout = () => {
-        postLogout(UserToken, {
-            onError: (error) => {
-                alert(error.message);
-            },
-            onSuccess: () => {
-                removeToken();
-                navigate('/');
-            }
-        })
-    }
-    return { onLogout };
-}
+  const onLogout = () => {
+    postLogout({
+      onSuccess: () => {
+        removeToken();
+        navigate('/');
+      },
+    });
+  };
+  return { onLogout };
+};
