558dcd50626d59e5aba47c5c8739cb13b69e9175
dxwwan <kkw1700@gmail.com>
2024-01-24 05:17:17 +0900

    refactor(apply): 소속대학 셀렉터 컴포넌트 추가

--- service-apply/src/components/apply/ApplySelector.tsx
+++ service-apply/src/components/apply/ApplySelector.tsx
@@ -1,23 +1,34 @@
 import { SelectHTMLAttributes, useId } from 'react';
 import { Txt } from '@quokka/design-system';
+import {
+  AffiliationList,
+  AFFILIATION_LIST,
+} from 'service-apply/src/constants/affiliation';
 
 interface ParkingSection {
   sectionNumber: number;
   sectionMajor: string;
 }
 
-interface SelectorProps extends SelectHTMLAttributes<HTMLSelectElement> {
+interface SectionSelectorProps extends SelectHTMLAttributes<HTMLSelectElement> {
   label: string;
   type: string;
   options: ParkingSection[];
 }
 
-export const ApplySelector = ({
+interface AffiliationSelectorProps
+  extends SelectHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  type: string;
+  options: AffiliationList[];
+}
+
+export const SectionSelector = ({
   label,
   type,
   options,
   ...props
-}: SelectorProps) => {
+}: SectionSelectorProps) => {
   const id = useId();
 
   return (
@@ -34,6 +45,35 @@ export const ApplySelector = ({
         {options.map((options) => (
           <option key={options.sectionNumber} value={options.sectionNumber}>
             {options.sectionMajor}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export const AffiliationSelector = ({
+  label,
+  type,
+  options,
+  ...props
+}: AffiliationSelectorProps) => {
+  const id = useId();
+
+  return (
+    <div className="grid grid-cols-5 items-center">
+      <label htmlFor={id}>
+        {label}
+        {props.required && <Txt color="error">*</Txt>}
+      </label>
+      <select
+        {...props}
+        id={id}
+        className="p-2 border border-[#D9D9D9] col-span-4 rounded-md"
+      >
+        {options.map((options) => (
+          <option key={options.label} value={options.value}>
+            {options.label}
           </option>
         ))}
       </select>
