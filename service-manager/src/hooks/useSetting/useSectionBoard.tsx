816ace7550a977a5d9fae2b54151840d77e67ad9
dxwwan <kkw1700@gmail.com>
2024-01-25 12:42:37 +0900

    refactor(manager): canCreate를 조회하도록 변경

--- service-manager/src/hooks/useSetting/useSectionBoard.tsx
+++ service-manager/src/hooks/useSetting/useSectionBoard.tsx
@@ -3,17 +3,12 @@ import { useSettingEventsQuery } from '../react-query/useSetting';
 export const useSectionBoard = (pageIndex: number) => {
   const { coupon } = useSettingEventsQuery(pageIndex);
   let checkCoupon = coupon;
-  let canCreate = true;
   if (pageIndex !== 1) {
     const { coupon } = useSettingEventsQuery(1);
     checkCoupon = coupon;
   }
-  if (
-    coupon.couponEvents.some(
-      (event) => event.eventStatus === 'OPEN' || event.eventStatus === 'READY',
-    )
-  ) {
-    canCreate = false;
-  }
+  const canCreate = !coupon.couponEvents.some(
+    (event) => event.eventStatus === 'OPEN' || event.eventStatus === 'READY',
+  );
   return { coupon, canCreate };
 };
