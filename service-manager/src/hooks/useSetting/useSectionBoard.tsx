8895b3c4a283ce9f339148ed1f554eaaca18469a
2yunseong <dbsdltjd3701@naver.com>
2024-01-31 22:52:04 +0900

    fix: hook 규칙을 위배하는 코드 수정

--- service-manager/src/hooks/useSetting/useSectionBoard.tsx
+++ service-manager/src/hooks/useSetting/useSectionBoard.tsx
@@ -1,15 +1,11 @@
 import { useSettingEventsQuery } from '../react-query/useSetting';
 
 export const useSectionBoard = (pageIndex: number) => {
-  const { coupon } = useSettingEventsQuery(pageIndex);
-  // TODO: PLAESE REMOVE THIS CODE 6~10L
-  let checkCoupon = coupon;
-  if (pageIndex !== 1) {
-    const { coupon } = useSettingEventsQuery(1);
-    checkCoupon = coupon;
-  }
-  const canCreate = !coupon.couponEvents.some(
+  const { coupon: pageCoupon } = useSettingEventsQuery(pageIndex);
+  const { coupon: forCreateCoupon } = useSettingEventsQuery(1);
+
+  const canCreate = !forCreateCoupon.couponEvents.some(
     (event) => event.eventStatus === 'OPEN' || event.eventStatus === 'READY',
   );
-  return { coupon, canCreate };
+  return { coupon: pageCoupon, canCreate };
 };
