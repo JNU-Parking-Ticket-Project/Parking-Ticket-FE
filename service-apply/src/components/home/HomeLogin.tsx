f6a844475f448ccb76ca9a87063f1ea79f1d3355
2yunseong <dbsdltjd3701@naver.com>
2024-08-26 23:40:07 +0900

    refactor: 컴포넌트 분리

--- service-apply/src/components/home/HomeLogin.tsx
+++ service-apply/src/components/home/HomeLogin.tsx
@@ -1,6 +1,7 @@
 import { Button, InputText, Txt } from '@quokka/design-system';
 import { Link } from 'react-router-dom';
 import useLoginForm from '../../hooks/home/useLoginForm';
+import LoginInputBox from './LoginInputBox';
 
 export const HomeLogin = () => {
   const {
@@ -20,29 +21,21 @@ export const HomeLogin = () => {
           신청 폼 작성하기
         </Txt>
         <div className="flex flex-col gap-3 items-end">
-          <div className="w-full">
-            <InputText
-              designType="box"
-              type="text"
-              placeholder="이메일"
-              name="email"
-              className="w-full p-4"
-              value={email}
-              onChange={onChangeEmail}
-            />
-          </div>
-          <div className="w-full">
-            <InputText
-              designType="box"
-              type="password"
-              placeholder="비밀번호"
-              name="password"
-              className="w-full p-4"
-              value={password}
-              onChange={onChangePassword}
-            />
-          </div>
-          <Link to={'/password-reset'}>
+          <LoginInputBox
+            type="text"
+            placeholder="이메일"
+            name="email"
+            value={email}
+            onChange={onChangeEmail}
+          />
+          <LoginInputBox
+            type="password"
+            placeholder="비밀번호"
+            name="password"
+            value={password}
+            onChange={onChangePassword}
+          />
+          <Link to="/password-reset">
             <Txt color="secondary">비밀번호 찾기</Txt>
           </Link>
           <Button
