49a1df0ea1a868f3b4c66a369854321b2313d217
minbo <smb5174@naver.com>
2024-10-28 15:50:16 +0900

    feat: 모달 닫기 버튼 추가

--- service-manager/src/components/announcement/AnnouncementImgContainer.tsx
+++ service-manager/src/components/announcement/AnnouncementImgContainer.tsx
@@ -18,7 +18,7 @@ interface AnnouncementImgProps {
 interface AnnouncementImgModalProps {
   image: string;
   isOpen: boolean;
-  setIsOpen: () => void;
+  setClose: () => void;
 }
 interface AnnouncementAddImgProps
   extends Pick<AnnouncementImgContainerProps, 'setImages'> {}
@@ -81,7 +81,7 @@ export function AnnouncementImg({ image, setImages }: AnnouncementImgProps) {
         <AnnouncementImgModal
           isOpen={isOpen}
           image={image}
-          setIsOpen={() => setIsOpen(false)}
+          setClose={() => setIsOpen(false)}
         />
       )}
     </>
@@ -91,13 +91,13 @@ export function AnnouncementImg({ image, setImages }: AnnouncementImgProps) {
 export function AnnouncementImgModal({
   image,
   isOpen,
-  setIsOpen,
+  setClose,
 }: AnnouncementImgModalProps) {
   return (
     <Modal
       className="announcementImg-modal"
       isOpen={isOpen}
-      onRequestClose={setIsOpen}
+      onRequestClose={setClose}
       overLayCss={{
         display: 'flex',
         justifyContent: 'center',
@@ -108,8 +108,15 @@ export function AnnouncementImgModal({
       contentCss={{
         borderRadius: '1rem',
         width: '800px',
+        position: 'relative',
       }}
     >
+      <button
+        onClick={setClose}
+        className="text-6xl absolute -right-16 -top-16"
+      >
+        X
+      </button>
       <img alt="공지사항" src={image} className="mx-auto w-fit max-h-screen" />
     </Modal>
   );
