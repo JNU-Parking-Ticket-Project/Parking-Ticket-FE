c28922f05fbfee6a6951bc2e31f3158464adea7a
minbo <smb5174@naver.com>
2025-02-19 00:16:08 +0900

    feat: 이메일 전송 기능 추가

--- service-manager/src/hooks/react-query/useRegistration.tsx
+++ service-manager/src/hooks/react-query/useRegistration.tsx
@@ -1,5 +1,9 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { getAllRegistration } from '../../apis/registration.apis';
+import {
+  MutateOptions,
+  useMutation,
+  useSuspenseQuery,
+} from '@tanstack/react-query';
+import { getAllRegistration, postEmail } from '../../apis/registration.apis';
 
 export const useAllRegistrationQuery = (eventId: string) => {
   const { data } = useSuspenseQuery({
@@ -7,4 +11,43 @@ export const useAllRegistrationQuery = (eventId: string) => {
     queryFn: () => getAllRegistration(eventId),
   });
   return { registrations: data };
+};
+
+export const useTransmitEmail = () => {
+  const { postEmail } = useEmailTransmitMutate();
+
+  const onEmailTransmit = (eventId: string) => {
+    postEmail(eventId, {
+      onError: (error) => {
+        alert(error.message);
+      },
+      onSuccess: (data) => {
+        alert(data.message);
+      },
+    });
+  };
+  return {
+    onEmailTransmit,
+  };
+};
+
+export const useEmailTransmitMutate = () => {
+  const { mutate } = useMutation({
+    mutationKey: ['email'],
+    mutationFn: postEmail,
+  });
+
+  return {
+    postEmail: (
+      eventId: string,
+      mutateOption?: Omit<
+        MutateOptions<{ message: string }, Error, unknown>,
+        'onSettled'
+      >,
+    ) => {
+      mutate(eventId, {
+        ...mutateOption,
+      });
+    },
+  };
 };
